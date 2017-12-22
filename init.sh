377366ac95e7506230c199d19cb1a204356df3b0
Debdut Karmakar <iawaiponly@gmail.com>
2017-12-22 13:09:49 +0530

    First Commit

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+sudo apt remove rhythmbox totem* gnome-software gnome-mahjongg gnome-sudoku gnome-mines aisleriot libreoffice* transmission-*
+sudo apt autoremove
+sudo add-apt-repository ppa:hvr/ghc
+sudo add-apt-repository ppa:plt/racket
+sudo add-apt-repository ppa:libreoffice/ppa
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+echo "deb http://download.onlyoffice.com/repo/debian squeeze main" >> file
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo add-apt-repository ppa:mozillateam/firefox-next
+sudo add-apt-repository ppa:webupd8team/atom
+sudo add-apt-repository ppa:webupd8team/brackets
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo add-apt-repository ppa:saiarcot895/chromium-dev
+sudo add-apt-repository ppa:ubuntu-wine/ppa
+sudo add-apt-repository ppa:brightbox/ruby-ng
+sudo wget -O - https://tagplus5.github.io/vscode-ppa/ubuntu/gpg.key | sudo apt-key add -
+sudo wget -O /etc/apt/sources.list.d/vscode.list https://tagplus5.github.io/vscode-ppa/ubuntu/vscode.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo add-apt-repository ppa:numix/ppa
+sudo add-apt-repository ppa:noobslab/icons2
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y mongodb-org
+sudo apt install nodejs php7.0 ruby-full haskell-platform python-pip python3-pip python-virtualenv emacs25 atom code code-insiders racket vim-gnome qbittorrent unity-tweak-tool gparted chromium-browser zeal zsh gdebi numix-gtk-theme numix-icon-theme-circle uniform-icons powerline screenfetch
+sudo apt autoremove
+sudo apt upgrade
+sudo chsh -s /bin/zsh
---
+++ init2.sh
@@ -0,0 +1,10 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+wget "https://raw.githubusercontent.com/caiogondim/bullet-train.zsh/master/bullet-train.zsh-theme"
+mv bullet-train.zsh-theme .oh-my-zsh/themes/
+curl https://sh.rustup.rs -sSf | sh
+echo "screenfetch" >> .zshrc
+apm install atom-live-server atom-beautify script teletype file-icons language-babel atom-ide-ui ide-typescript ide-flowtype ide-python
+pip3 install ipython jupyter
+wget "https://raw.githubusercontent.com/todylu/monaco.ttf/master/monaco.ttf"
+mkdir .fonts
+mv monaco.ttf .fonts/
